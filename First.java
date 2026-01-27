27ecf2869d74db8cc78550484ebab803632eb1d9
Deepak Singh <deepakrajput.ds2008@gmail.com>
2026-01-28 00:46:21 +0530

    Add First Java program

---
+++ First.java
@@ -0,0 +1,9 @@
+class First
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World ");
+        System.out.println("My name is Deepak Singh Rajput and i am 18 years old ");      
+        System.out.println("Today i am starting Java programming  ");          
+    }   
+}
