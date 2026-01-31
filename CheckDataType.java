06063312d31a29b464e97f71ca38dbdf6bffd6f9
Deepak Singh <deepakrajput.ds2008@gmail.com>
2026-02-01 00:24:55 +0530

    Add Java programs to check data types

---
+++ CheckDataType.java
@@ -0,0 +1,39 @@
+public class CheckDataType
+{
+    public static void main(String[] args)
+    {
+        boolean b1 = true;
+        boolean b2 = false;
+        System.out.println("Boolean values:");
+        System.out.println("b1: " + b1);
+        System.out.println("b2: " + b2);
+
+        byte by1 = 1;
+        System.out.println("Byte value:");
+        System.out.println("by1: " + by1);
+
+        short s = 10;
+        System.out.println("Short value:");
+        System.out.println("s: " + s);
+
+        int i = 100;
+        System.out.println("Integer value:");
+        System.out.println("i: " + i);
+
+        long l = 123456L;
+        System.out.println("Long value:");
+        System.out.println("l: " + l);
+
+        float f = 0.1f;
+        System.out.println("Float value:");
+        System.out.println("f: " + f);
+
+        double d = 12.5;
+        System.out.println("Double value:");
+        System.out.println("d: " + d);
+
+        char character = 'a';
+        System.out.println("Character value:");
+        System.out.println("character: " + character);
+    }
+}
