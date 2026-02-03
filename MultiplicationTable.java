cc687dfbdd27f9eb83a49e5d9fa2f0f76354a262
Deepak Singh <deepakrajput.ds2008@gmail.com>
2026-02-04 00:12:19 +0530

    Add Java program to print multiplication table

---
+++ MultiplicationTable.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/*
+ * Program Name: Tables
+ * Description : Prints multiplication table of a given number.
+ */
+public class Tables {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Taking input from user
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        // Printing multiplication table
+        System.out.println("\nMultiplication Table of " + number + ":");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+
+        // Closing scanner
+        sc.close();
+    }
+}
