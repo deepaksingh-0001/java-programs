a90fb0bd19d4eb95a65e960a4b47b271339e5f35
Deepak Singh <deepakrajput.ds2008@gmail.com>
2026-02-26 16:27:05 +0530

    Add Java program to create and print a first string

---
+++ FirstString.java
@@ -0,0 +1,11 @@
+public class FirstString 
+{
+    // Main Function
+   public static void main(String args[])
+   {
+    // creating Java string using a new keyword
+    String str = new String("hello string");
+
+    System.out.println(str);
+    }
+}
